63c4bf2aa343dccbd9938149aded21a00f039b87
mayang0001 <mayang0001@sina.com>
2017-12-31 11:33:33 +0800

    add accumulate

--- algorithm.h
+++ algorithm.h
@@ -1,16 +1,28 @@
-#ifndef ITERATOR_H
-#define ITERATOR_H
+#ifndef ITERATOR_H_
+#define ITERATOR_H_
 
-#include <iostream>
+template <typename Iterator, typename T>
+T accumulate(Iterator begin, Iterator end, T init) {
+  for (; begin != end; ++begin) {
+    init = init + *begin;
+  }
+  return init;
+}
+
+template <typename Iterator, typename T, typename Fucntor>
+T accumulate(Iterator begin, Iterator end, T init, Functor binary_op) {
+  for (; begin != end; ++begin) {
+    init = binary_op(init, *begin);
+  }
+  return init;
+}
 
 template<typename Iterator, typename T> 
-Iterator find(Iterator begin, Iterator end, const T& val) {
-  for (Iterator iter = begin; iter != end; ++iter) {
-    if (*iter == val) {
-      return iter;
-    }
+Iterator find(Iterator begin, Iterator end, const T& value) {
+  while (begin != end && *begin != value) {
+    ++begin;
   }
-  return end;
+  return begin;
 }
 
 template<typename Iterator, typename Function>
