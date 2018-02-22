25af4f4aed732a53c2be47a9bb1423e5c6981c84
mayang0001 <mayang0001@sina.com>
2018-02-22 23:08:49 +0800

    add push heap

--- queue.h
+++ queue.h
@@ -38,4 +38,24 @@ class Queue {
   container_type container_;
 };
 
+
+// Functions for priority_queue
+
+template <typename RandomAccessIterator>
+void PushHeap(RandomAccessIterator first, RandomAccessIterator last) {
+  PushHeap(first, (last - first - 1), 0, *(last - 1));
+}
+
+template <typename RandomAccessIterator, typename Distance, typename T>
+void PushHeap(RandomAccessIterator first, Distance hole_idx, Distance top_idx, 
+              const T& value) {
+  Distance parent = (hole_idx - 1) / 2;
+  while (hole_idx > top_idx && *(first + parent) < value) {
+    *(first + hole_idx) = value;
+    hole_idx = parent;
+    parent = (hole_idx - 1) / 2;
+  }
+  *(first + hole_idx) = value;
+}
+
 #endif // QUEUE_H_
