0f96a2f15f153b1ae2b9c94f3310bd89de2d2286
mayang0001 <mayang0001@sina.com>
2018-02-23 23:05:52 +0800

    git study

--- iterator.h
+++ iterator.h
@@ -1,14 +1,31 @@
+#ifndef ITERATOR_H_
+#define ITERATOR_H_
+
 template <typename Iterator>
 struct iterator_traits {
-
+  using iterator_category = typename Iterator::iterator_category;
+  using value_type = typename Iterator::value_type;
+  using diffenence_type = typename Iterator::difference_type;
+  using reference = typename Iterator::reference;
+  using pointer = typename Iterator::pointer;
 };
 
 template <typename T>
 struct iterator_traits<T*> {
-
+  using iterator_category = random_access_iterator_tag;
+  using value_type = T;
+  using difference_type = otfdiff_t;
+  using reference = T&;
+  using pointer = T*;
 };
 
 template <typename T>
 struct iterator_traits<const T*> {
-
+  using iterator_category = random_access_iterator_tag;
+  using value_type = T;
+  using difference_type = otfdiff_t;
+  using reference = const T&;
+  using pointer = const T*;
 };
+
+#endif // ITERATOR_H_
