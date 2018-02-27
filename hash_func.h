54f9ab9944f06b2671aded18998b9e63b3f116a1
mayang0001 <mayang0001@sina.com>
2018-02-27 22:00:57 +0800

    add hash functions

--- hash_func.h
+++ hash_func.h
@@ -5,10 +5,58 @@ template <typename T>
 struct Hash {};
 
 template <>
+struct Hash<char> {
+  size_t operator()(char val) {
+    return val;
+  }
+};
+
+template <>
+struct Hash<unsigned char> {
+  size_t operator()(unsigned char val) {
+    return val;
+  }
+};
+
+template <>
+struct Hash<short> {
+  size_t operator()(short val) {
+    return val;
+  }
+};
+
+template <>
+struct Hash<unsigned short> {
+  size_t operator()(unsigned short val) {
+    return val;
+  }
+};
+
+template <>
 struct Hash<int> {
   size_t operator()(int val) {
     return val;
   }
 };
 
+template <>
+struct Hash<unsigned int> {
+  size_t operator()(unsigned int val) {
+    return val;
+  }
+};
+
+template <>
+struct Hash<long> {
+  size_t operator()(long val) {
+    return val;
+  }
+};
+
+template <>
+struct Hash<unsigned long> {
+  size_t operator()(unsigned long val) {
+    return val;
+  }
+};
 #endif // HASH_FUNC_H_
