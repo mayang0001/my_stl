dd5aecc560451fac45edee0e7e64f3df9f9a2912
mayang0001 <mayang0001@sina.com>
2018-02-28 12:52:04 +0800

    finish hash functions

--- hash_func.h
+++ hash_func.h
@@ -4,59 +4,67 @@
 template <typename T>
 struct Hash {};
 
+inline size_t HashString(const char* s) {
+  unsigned long h = 0;
+  for (; *s; ++s) {
+    h = h *5 + *s;
+  }
+  return size_t(h);
+}
+
+template <>
+struct Hash<char*> {
+  size_t operator()(char* val) { return HashString(val); }
+};
+
+template <>
+struct Hash<const char*> {
+  size_t operator()(const char* val) { return HashString(val); }
+};
+
 template <>
 struct Hash<char> {
-  size_t operator()(char val) {
-    return val;
-  }
+  size_t operator()(char val) { return val; }
 };
 
 template <>
 struct Hash<unsigned char> {
-  size_t operator()(unsigned char val) {
-    return val;
-  }
+  size_t operator()(unsigned char val) { return val; }
+};
+
+template <>
+struct Hash<signed char> {
+  size_t operator()(signed char val) { return val; }
 };
 
 template <>
 struct Hash<short> {
-  size_t operator()(short val) {
-    return val;
-  }
+  size_t operator()(short val) { return val; }
 };
 
 template <>
 struct Hash<unsigned short> {
-  size_t operator()(unsigned short val) {
-    return val;
-  }
+  size_t operator()(unsigned short val) { return val; }
 };
 
 template <>
 struct Hash<int> {
-  size_t operator()(int val) {
-    return val;
-  }
+  size_t operator()(int val) { return val; }
 };
 
 template <>
 struct Hash<unsigned int> {
-  size_t operator()(unsigned int val) {
-    return val;
-  }
+  size_t operator()(unsigned int val) { return val; }
 };
 
 template <>
 struct Hash<long> {
-  size_t operator()(long val) {
-    return val;
-  }
+  size_t operator()(long val) { return val; }
 };
 
 template <>
 struct Hash<unsigned long> {
-  size_t operator()(unsigned long val) {
-    return val;
-  }
+  size_t operator()(unsigned long val) { return val; }
 };
+
 #endif // HASH_FUNC_H_
